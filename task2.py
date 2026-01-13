b8e69c0108a7f7fb05f4ddfbe1b6a83131637799
Dhaygude Sakshi Mahadev <dhaygudesakshi3@gmail.com>
2026-01-13 21:31:46 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,9 @@
 print("This is Task 2")
+# Task 2 :Create a personalized greeting
+first_name=input("Enter a first name:")
+last_name=input("Enter a last name:")
+full_name=first_name + " " + last_name
+print(full_name)
+print(f" Hello, {full_name}! Welcome to the python program")
+
+
