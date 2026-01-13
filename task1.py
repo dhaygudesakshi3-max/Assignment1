0951e9682638e4ad5c1e496778800584266110d5
Dhaygude Sakshi Mahadev <dhaygudesakshi3@gmail.com>
2026-01-13 21:30:37 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,14 @@
 print("This is Task 1")
+# Task 1: Perform basic arithmetical operations
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+division=num1/num2
+print(f" Addition: {add}")
+print(f" Subtraction: {sub}")
+print(f" Multiplication: {mul}")
+print(f" Division: {division}")
+
+
